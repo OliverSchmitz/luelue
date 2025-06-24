41ec9794338c5f5c43c715a864d7095e437608e0
Oliver Schmitz <o.schmitz@uu.nl>
2025-06-24 18:42:50 +0200

    WIP CI

---
+++ hpx/build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+
+
+mkdir build
+pushd build
+
+if [[ "$target_platform" == "osx-64" ]]; then
+    # https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+    export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
+cmake \
+    -G"Ninja" \
+    ${CMAKE_ARGS} \
+    -D CMAKE_INSTALL_LIBDIR=lib \
+    -D PYTHON_EXECUTABLE="$PYTHON" \
+    -D HPX_WITH_EXAMPLES=FALSE \
+    -D HPX_WITH_MALLOC="tcmalloc" \
+    -D HPX_WITH_NETWORKING=FALSE \
+    -D HPX_WITH_TESTS=FALSE \
+    ..
+cmake --build . --config Release --parallel ${CPU_COUNT}
+cmake --install .
---
+++ hpx/run_test.bat
@@ -0,0 +1,10 @@
+pushd test
+
+cmake %CMAKE_ARGS% .
+if errorlevel 1 exit /b 1
+
+cmake --build . --config Release
+if errorlevel 1 exit /b 1
+
+Release\hello_hpx.exe
+if errorlevel 1 exit /b 1
